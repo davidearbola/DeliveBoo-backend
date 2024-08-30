ea04ff4c056f2bb5de5e5c548474ff3aa6ad7a36
MassimilianoRocco <massimilianorocco93@gmail.com>
2024-08-30 16:49:13 +0200

    Crud Product

--- app/Http/Controllers/Admin/ProductsController.php
+++ app/Http/Controllers/Admin/ProductsController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class ProductsController extends Controller
 {
@@ -24,8 +27,9 @@ class ProductsController extends Controller
 
     }
 
+  
     /**
-     * Show the form for creating a new resource.
+     * Show 
      */
     public function create()
     {
@@ -39,9 +43,25 @@ class ProductsController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreProductRequest $request)
     {
-        //
+        $user = Auth::user();
+        $restaurant = $user->restaurant;
+        
+        $data = $request->validated();
+        $data['restaurant_id'] = $restaurant->id;
+
+
+        $image_path = Storage::disk("public")->put('uploads', $data->image_path);
+        $data['image_path'] = $image_path;
+        
+
+        $newProduct = new Product();
+        $newProduct->fill($data);
+        $newProduct->save();
+        
+        return redirect()->route('admin.products.show', $newProduct->id);
+
     }
 
     /**
@@ -61,15 +81,36 @@ class ProductsController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $user = Auth::user();
+        $restaurant = $user->restaurant;
+        $product = Product::where('restaurant_id', $restaurant->id)->findOrFail($id);
+        
+        return view('admin.products.edit', compact('product'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateProductRequest $request, Product $product)
     {
-        //
+            
+        $data = $request->validated();
+
+        if ($data->has('image_path')) {
+
+            //Se il product già aveva un'immagine la si cancella e si mette la nuova
+            if($product->image_path){
+                Storage::disk('public')->delete($product->image_path);
+            }
+
+        // save the image
+        $image_path = Storage::disk("public")->put('uploads', $data['image_path']);
+        $data['image_path'] = $image_path;
+        }
+
+        $product->update($data);
+
+        return redirect()->route('admin.products.show', $product);
     }
 
     /**
@@ -77,6 +118,17 @@ class ProductsController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $user = Auth::user();
+        $restaurant = $user->restaurant;
+        $product = Product::where('restaurant_id', $restaurant->id)->findOrFail($id);
+
+        if($product->image_path){
+            Storage::disk('public')->delete($product->image_path);
+        }
+
+        $product->delete();
+
+        return redirect()->route('admin.products.index');
+        
     }
 }
--- app/Http/Requests/UpdateProductRequest.php
+++ app/Http/Requests/UpdateProductRequest.php
@@ -11,7 +11,7 @@ class UpdateProductRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,12 @@ class UpdateProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
-        ];
+            "name"=>"required|min:3|max:30",
+            "ingredients"=>"nullable|string|max:255|",
+            "price"=>"required|numeric|gt:-0.01|lt:10000|decimal:2",
+            "visible"=>"boolean",
+            "type"=>"string|required|in:Food,Bibite,Bevande Alcoliche,Dessert",
+            "image_path" =>"required|image"
+           ];
     }
 }
