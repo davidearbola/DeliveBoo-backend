5610d03ee1d12ae18482fb5adcbb1391276f0060
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-09-02 15:12:13 +0200

    show page

--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -1,2 +1,89 @@
-{{$product->name}} 
-{{$product->restaurant->name}}
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container box pb-3">
+        <h1 class="text-center display-4 fw-bold mt-4 pt-4 text-danger">INFO PIATTO</h1>
+
+        <div class="row mx-0 justify-content-center">
+            <div class="col-12 px-0 ">
+                <div class="row mx-0 my-3 justify-content-center">
+
+                    {{-- IMAGE --}}
+                    <div class="col-5 my-4 px-3">
+
+                        <div class="card imgBox border-3 border-white">
+                            <img src="{{ $product->image_path }}" class="card-img object-fit-fill"></img>
+                        </div>
+
+                    </div>
+
+                    {{-- INFO LIST --}}
+                    <div class="col-5 my-4 px-4 align-content-center">
+
+                        <div><span class="text-danger fw-bold">NOME : </span> {{ $product->name }}</div>
+                        @if ($product->type == 'Food' || $product->type == 'Dessert')
+                            <div><span class="text-danger fw-bold">INGREDIENTI : </span>{{ $product->ingredients }}</div>
+                        @endif
+                        <div><span class="text-danger fw-bold">TIPO : </span>{{ $product->type }}</div>
+                        <div><span class="text-danger fw-bold">PREZZO : </span>{{ $product->price }} €</div>
+                        <div>
+                            <span class="text-danger fw-bold">VISIBILE : </span>
+                            @if ($product->visible == 1)
+                                SI
+                            @else
+                                NO
+                            @endif
+                        </div>
+
+                    </div>
+
+                    {{-- BUTTONS --}}
+                    <div class="col-5 text-center mt-2">
+
+                        <a class="text-decoration-none" href="{{ route('admin.products.index') }}">
+                            <button class="btn fw-bold bg-danger border-2 border-white mx-2">
+
+                                <i class="fa-solid fa-list-ul"></i> LISTA PIATTI
+
+                            </button>
+                        </a>
+
+                        <a class="text-decoration-none" href="{{ route('admin.products.edit', $product->id) }}">
+                            <button class="btn fw-bold bg-warning px-3 border-2 border-white mx-2">
+
+                                <i class="fa-solid fa-pen-to-square"></i> MODIFICA
+
+                            </button>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <style>
+        .box {
+            background: linear-gradient(90deg, rgba(35, 35, 35, 1) 5%, rgba(26, 130, 93, 1) 50%, rgba(35, 35, 35, 1) 95%);
+            border-radius: 15px;
+            color: white;
+        }
+
+        .imgBox {
+            transition: transform 0.4s ease-in-out;
+        }
+
+        .imgBox:hover {
+            transform: scale(1.1);
+        }
+
+        a {
+            color: black;
+        }
+
+        a:hover {
+            cursor: pointer;
+            color: white;
+        }
+    </style>
+@endsection
