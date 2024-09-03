d433e1f241324ef3911d4d4a6623a0c3654278d6
Daniele-Taullaraj <danieletaullaraj@gmail.com>
2024-09-03 17:00:44 +0200

    add product api controller

---
+++ app/Http/Controllers/Api/ProductsController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ProductsController extends Controller
+{
+    public function index(string $restaurants_id)
+    {
+        $validator = Validator::make(['restaurant_id' => $restaurants_id], [
+            'restaurant_id' => 'integer|exists:restaurants,id',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => 'Invalid restaurant ID.',
+            ], 400); // 400 Bad Request
+        }
+
+        $product = Product::where('restaurant_id', $restaurants_id)->get();
+        return response()->json([
+            'product' => $product,
+        ]);
+    }
+
+
+
+
+    // aggiungere validazione del id ristorante e anche dei piatti se realmente sono contenuti in esso
+    public function show(string $restaurants_id, string $product_id)
+    {
+        $product = Product::where('restaurant_id', $restaurants_id)->where('id', $product_id)->get();
+
+        return response()->json([
+            'product' => $product,
+        ]);
+    }
+}
