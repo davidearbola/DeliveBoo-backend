0995f883b0d823384918162c6c035cb17ba4d689
Daniele-Taullaraj <danieletaullaraj@gmail.com>
2024-09-03 16:59:44 +0200

    add route api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\CategoriesController;
+use App\Http\Controllers\Api\ProductsController;
+use App\Http\Controllers\Api\RestaurantsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('restaurants', [RestaurantsController::class, 'index']);
+Route::get('restaurants/{restaurants_id}', [RestaurantsController::class, 'show']);
+
+Route::get('restaurants/{restaurants_id}/products', [ProductsController::class, 'index']);
+Route::get('restaurants/{restaurants_id}/products/{products_id}', [ProductsController::class, 'show']);
+
+
+
+
+
+Route::get('categories', [CategoriesController::class, 'index']);
