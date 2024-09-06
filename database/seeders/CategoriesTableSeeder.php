58b4979683d6a1779a77000b97ad667c7bce3f2e
davidearbola <davidearbola@gmail.com>
2024-09-06 16:43:49 +0200

    media-category

--- database/seeders/CategoriesTableSeeder.php
+++ database/seeders/CategoriesTableSeeder.php
@@ -14,18 +14,18 @@ class CategoriesTableSeeder extends Seeder
     public function run(): void
     {
         DB::table('categories')->insert([
-            ['name' => 'Pizza'],
-            ['name' => 'Fast Food'],
-            ['name' => 'Hamburger'],
-            ['name' => 'Italiano'],
-            ['name' => 'Panini'],
-            ['name' => 'Sushi'],
-            ['name' => 'Gelato'],
-            ['name' => 'Vegano'],
-            ['name' => 'Kebab'],
-            ['name' => 'Piadine'],
-            ['name' => 'Cinese'],
-            ['name' => 'Messicano'],
+            ['name' => 'Pizza', 'image_path' => 'https://raw.githubusercontent.com/davidearbola/prod-img-deliveboo/be31576b06c4ce70dad370418aaec3393093d5cb/pizza-cat.png'],
+            ['name' => 'Fast Food', 'image_path' => "https://raw.githubusercontent.com/davidearbola/prod-img-deliveboo/be31576b06c4ce70dad370418aaec3393093d5cb/fastfood-cat.png"],
+            ['name' => 'Hamburger', 'image_path' => "https://raw.githubusercontent.com/davidearbola/prod-img-deliveboo/be31576b06c4ce70dad370418aaec3393093d5cb/hamb-cat.png"],
+            ['name' => 'Italiano', 'image_path' => "https://raw.githubusercontent.com/davidearbola/prod-img-deliveboo/be31576b06c4ce70dad370418aaec3393093d5cb/spaghetti-cat.png"],
+            ['name' => 'Panini', 'image_path' => "https://raw.githubusercontent.com/davidearbola/prod-img-deliveboo/be31576b06c4ce70dad370418aaec3393093d5cb/panini-cat.png"],
+            ['name' => 'Sushi', 'image_path' => "https://raw.githubusercontent.com/davidearbola/prod-img-deliveboo/be31576b06c4ce70dad370418aaec3393093d5cb/sushi-cat.png"],
+            ['name' => 'Gelato', 'image_path' => "https://raw.githubusercontent.com/davidearbola/prod-img-deliveboo/be31576b06c4ce70dad370418aaec3393093d5cb/gelato-cat.png"],
+            ['name' => 'Vegano', 'image_path' => "https://raw.githubusercontent.com/davidearbola/prod-img-deliveboo/be31576b06c4ce70dad370418aaec3393093d5cb/vegan-cat.png"],
+            ['name' => 'Kebab', 'image_path' => "https://raw.githubusercontent.com/davidearbola/prod-img-deliveboo/be31576b06c4ce70dad370418aaec3393093d5cb/kebab-cat.png"],
+            ['name' => 'Piadine', 'image_path' => "https://raw.githubusercontent.com/davidearbola/prod-img-deliveboo/be31576b06c4ce70dad370418aaec3393093d5cb/piadina-cat.png"],
+            ['name' => 'Cinese', 'image_path' => "https://raw.githubusercontent.com/davidearbola/prod-img-deliveboo/be31576b06c4ce70dad370418aaec3393093d5cb/cinese-cat.png"],
+            ['name' => 'Messicano', 'image_path' => "https://raw.githubusercontent.com/davidearbola/prod-img-deliveboo/be31576b06c4ce70dad370418aaec3393093d5cb/messicano-cat.png"],
         ]);
     }
 }
