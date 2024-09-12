721ec1d5ae267ec599b5ff7c5b90a8d8b8b5c84f
davidearbola <davidearbola@gmail.com>
2024-09-12 16:58:51 +0200

    seeder with faker

--- database/migrations/2024_08_21_103452_create_restaurants_table.php
+++ database/migrations/2024_08_21_103452_create_restaurants_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('restaurants', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('slug')->nullable();
             $table->text('description');
             $table->string('phone');
             $table->string('address');
