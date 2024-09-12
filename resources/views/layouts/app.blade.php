d03e23503107c06ecceb273ae00faf91dcccb2cc
davidearbola <davidearbola@gmail.com>
2024-09-12 10:32:07 +0200

    name app e favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/x-icon" href={{ asset('images/DeliveBoo-Photoroom.png') }}>
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
@@ -26,44 +27,47 @@
         <header class="p-3 text-white d-flex align-items-center" id="my_headerLayout">
             <div class="container-fluid h-auto ">
                 <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                    <a href="http://localhost:5173" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-center text-decoration-none">
-                        <img src={{asset('images/DeliveBoo-Photoroom.png')}} alt="logo DeliveBoo" id="my_logo">
+                    <a href="http://localhost:5173"
+                        class="d-flex align-items-center mb-2 mb-lg-0 text-white text-center text-decoration-none">
+                        <img src={{ asset('images/DeliveBoo-Photoroom.png') }} alt="logo DeliveBoo" id="my_logo">
                     </a>
-    
+
                     <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                         <li><a href="http://localhost:5173/#video" class="nav-link px-2 text-white">Home</a></li>
-                        <li><a href="http://localhost:5173/#ristoranti" class="nav-link px-2 text-white">Lista Ristoranti</a></li>
-                        <li><a href="http://localhost:5173/#servizi" class="nav-link px-2 text-white">Cosa offriamo</a></li>
-                        <li><a href="http://localhost:5173/#lavora" class="nav-link px-2 text-white">Lavora con noi</a></li>
+                        <li><a href="http://localhost:5173/#ristoranti" class="nav-link px-2 text-white">Lista
+                                Ristoranti</a></li>
+                        <li><a href="http://localhost:5173/#servizi" class="nav-link px-2 text-white">Cosa offriamo</a>
+                        </li>
+                        <li><a href="http://localhost:5173/#lavora" class="nav-link px-2 text-white">Lavora con noi</a>
+                        </li>
                     </ul>
-    
+
                     <div class="">
                         @guest
-                        <a  href="{{ route('auth')}}">
-                            <button type="button" class="btn btn-warning my_button">Login/Registrati</button>
-                        </a>
-                        @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->restaurant->name }}
+                            <a href="{{ route('auth') }}">
+                                <button type="button" class="btn btn-warning my_button">Login/Registrati</button>
                             </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item"
-                                    href="{{ route('admin.dashboard') }}">{{ __('Dashboard') }}</a>
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->restaurant->name }}
                                 </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('admin.dashboard') }}">{{ __('Dashboard') }}</a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
 
                     </div>
                 </div>
@@ -82,8 +86,8 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent"> --}}
 
-                    <!-- Right Side Of Navbar -->
-                    {{-- <ul class="navbar-nav ml-auto">
+        <!-- Right Side Of Navbar -->
+        {{-- <ul class="navbar-nav ml-auto">
                         <!-- Authentication Link -->
                         @guest
                             <li class="nav-item">
@@ -112,7 +116,7 @@
                             </li>
                         @endguest
                     </ul> --}}
-                {{-- </div>
+        {{-- </div>
             </div>
         </nav> --}}
 
@@ -121,7 +125,7 @@
         </main>
     </div>
 
-    @yield("pageScript")
+    @yield('pageScript')
 </body>
 
 </html>
